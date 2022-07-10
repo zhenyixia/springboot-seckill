d3ca2df9a115921016ec0ac7ddd05c8206292a12
zhenyixia <zyx_2017@163.com>
2022-07-10 20:57:19 +0800

    增加启动rabbitmq 与 redis提示 增加登陆页面地址

--- src/main/java/com/jesper/seckill/MainApplication.java
+++ src/main/java/com/jesper/seckill/MainApplication.java
@@ -1,12 +1,26 @@
 package com.jesper.seckill;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication{
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+  public static void main(String[] args){
+
+    SpringApplication.run(MainApplication.class, args);
+  }
+
+  // 需要先启动 rabbitmq与 redis
+  // 1,  cd \"D:\\servers\\RabbitMQ Server\\rabbitmq_server-3.10.5\\sbin\" & d:
+  // 2,  set ERLANG_HOME=D:\servers\Erlang
+  // 3, rabbitmq-server.bat
+
+  // 启动 redis-server 到开始屏幕上点击
+
+  // 首页： http://localhost:8080/login/to_login
+
 
 }
